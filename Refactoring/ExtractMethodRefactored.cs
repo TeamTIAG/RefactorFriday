db1e37be82190b90c8cdf0c3713062203ee09f8d
Douglas Rogers <douglas.s.rogers.ctr@navy.mil>
2023-08-11 10:42:07 -0400

    Add project files.

---
+++ Refactoring/ExtractMethodRefactored.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Refactoring
+{
+    class ExtractMethodRefactored
+    {
+        public string ComposeAddressRefactored(string name, string address, string state, string city, string zip, int workPhone, int homePhone)
+        {
+            var builder = new StringBuilder();
+
+            builder.AppendLine(name);
+            builder.AppendLine(address);
+            builder.Append(city).Append(", ");
+            builder.Append(state).Append(", ");
+            builder.AppendLine(zip);
+
+            string workNumber = formatPhoneNumber(workPhone);
+            string homeNumber = formatPhoneNumber(homePhone);
+
+            builder.Append("Work Phone: ").AppendLine(workNumber);
+            builder.Append("Home Phone: ").AppendLine(homeNumber);
+
+            return builder.ToString();
+        }
+
+        private static string formatPhoneNumber(int phoneNumber)
+        {
+            var asString = phoneNumber.ToString();
+            var countryCode = asString.Substring(0, 1);
+            var areaCode = asString.Substring(1, 3);
+            var prefix = asString.Substring(4, 3);
+            var lineNumber = asString.Substring(7, 4);
+            string phoneString = string.Join('-', new[] { countryCode, areaCode, prefix, lineNumber });
+
+            return phoneString;
+        }
+    }
+}
---
+++ Refactoring/ExtractMethodSmelly.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+
+namespace Refactoring
+{
+    public class ExtractMethodSmelly
+    {
+        public string ComposeAddress(string name, string address, string state, string city, string zip, int workPhone, int homePhone)
+        {
+            var builder = new StringBuilder();
+
+            builder.AppendLine(name);
+            builder.AppendLine(address);
+            builder.Append(city).Append(", ");
+            builder.Append(state).Append(", ");
+            builder.AppendLine(zip);
+
+            var workString = workPhone.ToString();
+            var countryCode = workString.Substring(0, 1);
+            var areaCode = workString.Substring(1, 3);
+            var prefix = workString.Substring(4, 3);
+            var lineNumber = workString.Substring(7, 4);
+
+            var homeString = homePhone.ToString();
+            var homeCountryCode = homeString.Substring(0, 1);
+            var homeAreaCode = homeString.Substring(1, 3);
+            var homePrefix = homeString.Substring(4, 3);
+            var homeLineNumber = homeString.Substring(7, 4);
+
+            builder.Append("Work Phone: ").AppendJoin('-', new[] { countryCode, areaCode, prefix, lineNumber });
+            builder.Append("Home Phone: ").AppendJoin('-', new[] { homeCountryCode, homeAreaCode, homePrefix, homeLineNumber });
+
+            return builder.ToString();
+        }
+    }
+}
