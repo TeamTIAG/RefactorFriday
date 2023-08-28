e59360643388ab94827576be58dc24e010bcd12f
Douglas Rogers <douglas.s.rogers.ctr@navy.mil>
2023-08-28 12:08:08 -0400

    Add ExtractVariable project

---
+++ ExtractVariable/ExtractVariableRefactored.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExtractVariable
+{
+    class ExtractVariableRefactored
+    {
+        public string RefactoredPhoneNumber(string phoneNumber)
+        {
+            string countryCode = phoneNumber.Substring(0, 1);
+            string areaCode = phoneNumber.Substring(1, 3);
+            string prefix = phoneNumber.Substring(4, 3);
+            string lineNumber = phoneNumber.Substring(7, 4);
+            string phoneString = string.Join('-',
+                new[] { countryCode, areaCode, prefix, lineNumber });
+
+            return phoneString;
+        }
+    }
+}
---
+++ ExtractVariable/ExtractVariableSmelly.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ExtractVariable
+{
+    public class ExtractVariableSmelly
+    {
+        public string SmellyPhoneNumber(string phoneNumber)
+        {
+            string phoneString = string.Join('-', 
+                new[] { 
+                    //Country Code
+                    phoneNumber.Substring(0, 1),
+                    //Area Code
+                    phoneNumber.Substring(1, 3), 
+                    //Prefix
+                    phoneNumber.Substring(4, 3),  
+                    //Line Number
+                    phoneNumber.Substring(7, 4) });
+
+            return phoneString;
+        }
+    }
+}
